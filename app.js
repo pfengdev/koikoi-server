d86fe11bf8a6fc4893b5464dd81c25cdf6795c67
Peter Feng <pfengdev@gmail.com>
2019-01-29 15:16:03 -0800

    Handling player connect/disconnect. Renaming public/js/koikoi.js to public/js/index.js

--- app.js
+++ app.js
@@ -2,13 +2,41 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io').listen(http);
+var players = {};
 
+//How to stop someone from connecting?
 io.on('connection', function (socket) {
-  console.log('a user connected');
-  socket.on('disconnect', function () {
-    console.log('user disconnected');
-  });
+  if (Object.keys(players).length < 2) {
+  	console.log('a user connected: ' + socket.id);
+  	players[socket.id] = {
+  		playerId: socket.id
+  	};
+  	console.log(players);
+  	socket.emit('currentPlayers', players);
+  	socket.broadcast.emit('newPlayer', players[socket.id]);
+  	socket.on('disconnect', function () {
+    	disconnectPlayer(socket.id);
+  	});
+  } else {
+  	console.log('too many users');
+  	disconnectPlayer(socket.id);
+  }
 });
+
+//How to force someone to disconnect?
+function disconnectAllPlayers() {
+	console.log('user abandoned game. exiting game. disconnecting all users');
+	players = {};
+	Object.keys(players).forEach(function(id) {
+		io.emit('disconnect', id);
+	});
+}
+
+function disconnectPlayer(id) {
+	console.log('user disconnected: ' + id);
+    delete players[id];
+    io.emit('disconnect', id);
+}
 
 app.use(express.static(__dirname + '/public'))
 
